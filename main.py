e312e1c019dbe191d83ff31aa118862c5d1a58ee
Elina <elinasarkina17@gmail.com>
2024-03-05 20:50:16 +0100

    Pythonnew

---
+++ main.py
@@ -0,0 +1,9 @@
+username = input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это Маша!')
+elif username =='Элина':
+    print('О, это Элина')
+elif username == 'Мах':
+    print('Мах,привет')
+else:
+    print('Привет,', username)   
